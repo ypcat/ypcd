a65e3cb53f3ea5a17304afce7197534187190c7d
Pei-Lun Lee <ypcat6@gmail.com>
2012-06-06 17:26:36 +0800

    support python 2.4

--- ypcd.py
+++ ypcd.py
@@ -22,5 +22,6 @@ elif re.match('^\.\.+$', path):
     path = '/'.join(['..']*(len(path)-1))
 
 i,j,d = max((d.find(path), d.find(path)-len(d), d[4:]) for d in dirs)
-print path if os.path.isdir(path) or i < 0 else os.path.expanduser(d)
+#print path if os.path.isdir(path) or i < 0 else os.path.expanduser(d)
+print (os.path.isdir(path) or i < 0) and path or os.path.expanduser(d)
 
